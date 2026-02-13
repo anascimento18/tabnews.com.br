a8cfb5906d05f659b6dc24c27c1ac2fbbcfd12a3
anascimento18 <anascimento18@gmail.com>
2026-02-13 18:24:39 +0000

    gera pagina

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+        <h1>Olá,bom dia, tudo bem. TabNews!</h1>
+        <p>Bem-vindo ao meu site.</p>
+        </div>
+    );
+    }
+
+    export default Home;
