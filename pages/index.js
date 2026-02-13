f985aa7ce099277f66d293803dce64041b9b98fb
anascimento18 <anascimento18@gmail.com>
2026-02-13 19:28:55 +0000

    nova mensagem

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div>
         <h1>Boa tarde, tudo bem?</h1>
-        <p>Não Esquece que vc tem Marido, e ele te ama e se vc ama da uma risadinha, mesmo eu nao podendo ver!</p>
+        <p>Solta o preso!</p>
         </div>
     );
     }
